72467f6bb2b583a736140fea4efd4cd5ea243f16
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-12 13:55:14 -0500

    Added functions in LibraryItem to call saga functions on value updates

--- src/components/Profile/Library/LibraryItem.jsx
+++ src/components/Profile/Library/LibraryItem.jsx
@@ -2,18 +2,44 @@
 // Route to EditLibraryItem here
 
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function LibraryItem({libraryItem}) {
 
-    const [inEditView, setInEditView] = useState(false);
+    const dispatch = useDispatch();
 
-    const saveChanges = () => {
+    const [inLocationEditView, setInLocationEditView] = useState(false);
+    const [location, setLocation] = useState(libraryItem.googleMaps_placeId);
+
+    const goToEditView = () => {
+        setInLocationEditView(true);
+    }
+
+    const updateCondition = (e) => {
+        let newCondition = e.target.value;
+        dispatch({
+            type: 'SAGA_UPDATE_LIBRARY_CONDITION',
+            payload: newCondition
+        });
+    }
+
+    const updateLocation = () => {
+        // Send updated location
         setInEditView(false);
+        dispatch({
+            type: 'SAGA_UPDATE_LIBRARY_LOCATION',
+            payload: location
+        })
     }
 
     const deleteLibraryItem = () => {
+        // Confirm with user, then send delete request
         if(confirm('Delete from library?')){
             setInEditView(false);
+            dispatch({
+                type: 'SAGA_DELETE_LIBRARY_ITEM',
+                payload: libraryItem.id
+            });
         }
     }
 
@@ -21,14 +47,21 @@ export default function LibraryItem({libraryItem}) {
     return (        
         <div className="libraryItem">
             <img className="libraryItemImg" src={`https://covers.openlibrary.org/b/isbn/${libraryItem.isbn}-M.jpg`} />
-            {inEditView ? <p><button onClick={saveChanges}>Save Changes</button><button onClick={deleteLibraryItem}>Remove From Library</button></p> 
-                        : <p className="editButton" onClick={() => setInEditView(true)}>•••</p>}
             <p>{libraryItem.title} ({libraryItem.edition})</p>
             <p>by {libraryItem.author}</p>
             <p>{libraryItem.isbn}</p>
             <p>{libraryItem.publisher}, {libraryItem.year} {libraryItem.cover}</p>
-            {inEditView ? <p>edit</p> : <p>{libraryItem.condition} condition</p>}
-            {inEditView ? <p>edit</p> : <p>Found at {libraryItem.googleMaps_placeId}</p>}
+            <select defaultValue={libraryItem.condition} onChange={(e) => updateCondition(e)}>
+                <option value="F">Like New</option>
+                <option value="NF">Near Fine</option>
+                <option value="VG">Very Good</option>
+                <option value="G">Good</option>
+                <option value="FR">Fair</option>
+                <option value="P">Poor</option>
+            </select>
+            <p>{libraryItem.condition} condition</p>
+            {inLocationEditView ? <p><input onChange={(e) => setLocation(e.target.value)} value={location} /> <button onClick={updateLocation}>Save Changes</button></p> 
+                                : <p>Found at {libraryItem.googleMaps_placeId} <button onClick={goToEditView}>Edit Location</button></p>}
         </div>
     );
 }
