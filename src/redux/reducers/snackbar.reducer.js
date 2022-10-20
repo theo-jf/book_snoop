7050e5c54e14f64cfb209bccd42cd59c3da3d38d
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-20 18:21:47 -0500

    Restructured snackbar reducer into a single function

--- src/redux/reducers/snackbar.reducer.js
+++ src/redux/reducers/snackbar.reducer.js
@@ -1,48 +1,17 @@
-import { combineReducers } from 'redux';
-
-const isSnackbarOpen = (state = false, action) => {
+export default function snackbar(state = {
+                                            isOpen: false,
+                                            severity: '',
+                                            message: ''
+                                         },  action) {
     switch (action.type) {
-        case 'OPEN_SNACKBAR':
-            return true;
+        case 'SET_SNACKBAR':
+            return {
+                isOpen: action.payload.isOpen,
+                severity: action.payload.severity,
+                message: action.payload.message
+            };
         case 'CLOSE_SNACKBAR':
-            return false;
+            return {...state, isOpen: false};
     }
     return state;
-}
-
-const snackbarSeverity = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_SNACKBAR_ERROR':
-            return 'error';
-        case 'SET_SNACKBAR_SUCCESS':
-            return 'success';
-    }
-    return state;
-}
-
-    // Reducer variables to show / hide snackbars
-    // Add to library success / failure
-    // Add to wishlist success / failure
-    // Switch to library success / failure
-    // Edit location success / failure
-    // Saving new condition success / failure?? 
-    // Other places where an alert is too much (search errors?)
-    // Find all alerts and judge if they should be snackbars
-
-const snackbarMessage = (state = '', action) => {
-    switch (action.type) {
-        case 'LIBRARY_ADDITION_FAILURE':
-            return 'Error adding to library';
-        case 'LIBRARY_ADDITION_SUCCESS':
-            return 'Added to library';
-        case 'WISHLIST_ADDITION_FAILURE':
-            return 'Error adding to wishlist';
-        case 'WISHLIST_ADDITION_FAILURE':
-            return 'Added to wishlist';
-    }
-    return state;
-}
-
-export default combineReducers({
-
-  });
+};
