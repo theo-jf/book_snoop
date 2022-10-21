04d0558f73bf45acde5e842cc65d12a6cf9eeebe
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-21 14:04:45 -0500

    Added confirm password and zipcode inputs into registration

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,19 +4,34 @@ import { useDispatch, useSelector } from 'react-redux';
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [zipCode, setZipCode] = useState('');
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
   const registerUser = (event) => {
     event.preventDefault();
 
-    dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: username,
-        password: password,
-      },
-    });
+    if (password != confirmPassword) {
+      dispatch({
+        type: 'SET_SNACKBAR',
+        payload: {
+            isOpen: true,
+            severity: 'error',
+            message: "Passwords don't match"
+        }
+      });
+    } else if (password === confirmPassword) {
+      dispatch({
+        type: 'REGISTER',
+        payload: {
+          username: username,
+          password: password,
+        },
+      });
+    }
+
   }; // end registerUser
 
   return (
@@ -40,6 +55,17 @@ function RegisterForm() {
         </label>
       </div>
       <div>
+        <label htmlFor="zip">
+          Zip code (optional):
+          <input
+            type="text"
+            name="zip"
+            value={zipCode}
+            onChange={(event) => setZipCode(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
         <label htmlFor="password">
           Password:
           <input
@@ -50,6 +76,18 @@ function RegisterForm() {
             onChange={(event) => setPassword(event.target.value)}
           />
         </label>
+        <div>
+          <label htmlFor="confirmPassword">
+            Confirm Password:
+            <input
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              required
+              onChange={(event) => setConfirmPassword(event.target.value)}
+            />
+          </label>
+        </div>
       </div>
       <div>
         <input className="btn" type="submit" name="submit" value="Register" />
